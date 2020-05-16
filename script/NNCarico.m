bfc13fdf55aa5fd2c58853169405c0f957d024e3
Domenico <domenico.ragusa01@universitadipavia.it>
2020-05-16 22:56:50 +0200

    aggiunta rete RBWill

--- script/NNCarico.m
+++ script/NNCarico.m
@@ -4,7 +4,7 @@ close all
 
 load_dataset
 
-%% Neural Network
+%% Neural Network MLP
 setdemorandstream(491218342)
 x = {x1_id';x2_id'};
 t = y_id';
@@ -17,12 +17,29 @@ net = configure(net,x);
 [net, tr] = train(net,x,t);
 nntraintool
 
-
-%% Validazione
+% Validazione
 validation=[x1_val x2_val];
 y = net(validation');
 perf = perform(net,y_val,y)
  
+figure
+plot3(x1_val,x2_val,y+m,'rx')
+hold on
+plot3(x1_val, x2_val, y_val+trend_2, 'bo');
+grid on
+title('Carico elettrico italiano di Domenica')
+xlabel('Giorno dell''anno')
+ylabel('Ora del giorno')
+zlabel('Consumo elettrico')
+legend('dati della rete', 'dati da validare')
+
+%% Neural Network RB
+net = newrb(x,t,1.0,3,200);
+net.name='Will';
+y = sim(net,validation');
+perf = perform(net,y_val,y)
+
+% Validation
 figure
 plot3(x1_val,x2_val,y+m,'rx')
 hold on
