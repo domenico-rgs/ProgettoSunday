08aa8eaf30a587eacd3916d0ce759a0240a56d7a
AndreaGiuliani-Git <andrea.giuliani12@gmail.com>
2020-05-23 17:45:57 +0200

    ok

--- script/MPolinomi.m
+++ script/MPolinomi.m
@@ -12,8 +12,8 @@ x2_val=x2_val/24;
 [X1,X2] = meshgrid(linspace(0,1,365), linspace(0,1,24));
 
 %% Modello polinomiale terzo grado
-phi_B=[ones(n,1) x1_id x2_id x1_id.^2 x2_id.^2 x1_id.*x2_id x1_id.^3 x2_id.^3 (x1_id.^2).*x2_id];
-phi_B_ext = [ones(length(X1(:)),1) X1(:) X2(:) X1(:).^2 X2(:).^2 X1(:).*X2(:) X1(:).^3 X2(:).^3 (X1(:).^2).*X2(:)];
+phi_B=[ones(n,1) x1_id x2_id x1_id.^2 x2_id.^2  x1_id.*x2_id x1_id.^3 x2_id.^3 (x1_id.^2).*x2_id x1_id.*(x2_id.^2)];
+phi_B_ext = [ones(length(X1(:)),1) X1(:) X2(:) X1(:).^2 X2(:).^2 X1(:).*X2(:) X1(:).^3 X2(:).^3 (X1(:).^2).*X2(:) X1(:).*(X2(:).^2)];
 
 [theta_B, std_theta_B, q_B, y_hat_B, epsilon_B, SSR_B, y_hat_B_ext, y_hat_B_ext_mat] = identificazioneModello(phi_B, phi_B_ext, X1, y_id_dt);
 
@@ -21,9 +21,9 @@ phi_B_ext = [ones(length(X1(:)),1) X1(:) X2(:) X1(:).^2 X2(:).^2 X1(:).*X2(:) X1
 stampaModello('Modello polinomiale III grado',X1,X2,y_hat_B_ext_mat+p, x1_id, x2_id, y_id_dt+p)
 
 %% Modello polinomiale di quarto grado
-phi_C=[ones(n,1) x1_id x2_id x1_id.^2 x2_id.^2 x1_id.*x2_id x1_id.^3 x2_id.^3 (x1_id.^2).*x2_id x1_id.*(x2_id.^2) x1_id.^4 x2_id.^4 (x1_id.^2).*(x2_id.^2) (x1_id.^3).*x2_id x1_id.*(x2_id.^3)];
+phi_C=[ones(n,1) x1_id x2_id x1_id.^2 x2_id.^2 x1_id.*x2_id x1_id.^3 x2_id.^3 (x1_id.^2).*x2_id x1_id.*(x2_id.^2) x1_id.^4 x2_id.^4 (x1_id.^3).*x2_id x1_id.*(x2_id.^3) (x1_id.^2).*(x2_id.^2)];
 phi_C_ext = [ones(length(X1(:)),1) X1(:) X2(:) X1(:).^2 X2(:).^2 X1(:).*X2(:) X1(:).^3 X2(:).^3 (X1(:).^2).*X2(:)...
-    X1(:).*(X2(:).^2) X1(:).^4 X2(:).^4 (X1(:).^2).*(X2(:).^2) (X1(:).^3).*X2(:) X1(:).*(X2(:).^3)];
+    X1(:).*(X2(:).^2) X1(:).^4 X2(:).^4 (X1(:).^3).*X2(:) X1(:).*(X2(:).^3) (X1(:).^2).*(X2(:).^2)];
 
 [theta_C, std_theta_C, q_C, y_hat_C, epsilon_C, SSR_C, y_hat_C_ext, y_hat_C_ext_mat] = identificazioneModello(phi_C, phi_C_ext, X1, y_id_dt);
 
@@ -31,10 +31,10 @@ phi_C_ext = [ones(length(X1(:)),1) X1(:) X2(:) X1(:).^2 X2(:).^2 X1(:).*X2(:) X1
 stampaModello('Modello polinomiale IV grado',X1,X2,y_hat_C_ext_mat+p, x1_id, x2_id, y_id_dt+p)
 
 %% Modello polinomiale di quinto grado
-phi_D=[ones(n,1) x1_id x2_id x1_id.^2 x2_id.^2 x1_id.*x2_id x1_id.^3 x2_id.^3 (x1_id.^2).*x2_id x1_id.*(x2_id.^2) x1_id.^4 x2_id.^4 (x1_id.^2).*(x2_id.^2) (x1_id.^3).*x2_id x1_id.*(x2_id.^3) ...
-     x2_id.^5 (x1_id.^4).*x2_id x1_id.*(x2_id.^4) (x1_id.^3).*(x2_id.^2) (x1_id.^2).*(x2_id.^3)];
+phi_D=[ones(n,1) x1_id x2_id x1_id.^2 x2_id.^2 x1_id.*x2_id x1_id.^3 x2_id.^3 (x1_id.^2).*x2_id x1_id.*(x2_id.^2) x1_id.^4 x2_id.^4 (x1_id.^3).*x2_id x1_id.*(x2_id.^3) ...
+    (x1_id.^2).*(x2_id.^2) x1_id.^5 x2_id.^5 (x1_id.^4).*x2_id x1_id.*(x2_id.^4) (x1_id.^3).*(x2_id.^2) (x1_id.^2).*(x2_id.^3)];
 phi_D_ext = [ones(length(X1(:)),1) X1(:) X2(:) X1(:).^2 X2(:).^2 X1(:).*X2(:) X1(:).^3 X2(:).^3 (X1(:).^2).*X2(:) X1(:).*(X2(:).^2) X1(:).^4 X2(:).^4 ...
-    (X1(:).^2).*(X2(:).^2) (X1(:).^3).*X2(:) X1(:).*(X2(:).^3) X2(:).^5 (X1(:).^4).*X2(:) X1(:).*(X2(:).^4) (X1(:).^3).*(X2(:).^2) (X1(:).^2).*(X2(:).^3)];
+    (X1(:).^3).*X2(:) X1(:).*(X2(:).^3) (X1(:).^2).*(X2(:).^2) X1(:).^5 X2(:).^5 (X1(:).^4).*X2(:) X1(:).*(X2(:).^4) (X1(:).^3).*(X2(:).^2) (X1(:).^2).*(X2(:).^3)];
 
 [theta_D, std_theta_D, q_D, y_hat_D, epsilon_D, SSR_D, y_hat_D_ext, y_hat_D_ext_mat] = identificazioneModello(phi_D, phi_D_ext, X1, y_id_dt);
 
@@ -42,11 +42,11 @@ phi_D_ext = [ones(length(X1(:)),1) X1(:) X2(:) X1(:).^2 X2(:).^2 X1(:).*X2(:) X1
 stampaModello('Modello polinomiale V grado',X1,X2,y_hat_D_ext_mat+p, x1_id, x2_id, y_id_dt+p)
 
 %% Modello polinomiale di sesto grado
-phi_E=[ones(n,1) x1_id x2_id x1_id.^2 x2_id.^2 x1_id.*x2_id x1_id.^3 x2_id.^3 (x1_id.^2).*x2_id x1_id.*(x2_id.^2) x1_id.^4 x2_id.^4 (x1_id.^2).*(x2_id.^2) (x1_id.^3).*x2_id x1_id.*(x2_id.^3) ...
-     x1_id.^5 x2_id.^5 (x1_id.^4).*x2_id x1_id.*(x2_id.^4) (x1_id.^3).*(x2_id.^2) (x1_id.^2).*(x2_id.^3)...
+phi_E=[ones(n,1) x1_id x2_id x1_id.^2 x2_id.^2 x1_id.*x2_id x1_id.^3 x2_id.^3 (x1_id.^2).*x2_id x1_id.*(x2_id.^2) x1_id.^4 x2_id.^4 (x1_id.^3).*x2_id x1_id.*(x2_id.^3) ...
+     (x1_id.^2).*(x2_id.^2) x1_id.^5 x2_id.^5 (x1_id.^4).*x2_id x1_id.*(x2_id.^4) (x1_id.^3).*(x2_id.^2) (x1_id.^2).*(x2_id.^3)...
      x1_id.^6 x2_id.^6 x1_id.^5.*x2_id x1_id.*(x2_id.^5) (x1_id.^4).*(x2_id.^2) (x1_id.^2).*(x2_id.^4) (x1_id.^3).*(x2_id.^3)];
 phi_E_ext = [ones(length(X1(:)),1) X1(:) X2(:) X1(:).^2 X2(:).^2 X1(:).*X2(:) X1(:).^3 X2(:).^3 (X1(:).^2).*X2(:) X1(:).*(X2(:).^2) X1(:).^4 X2(:).^4 ...
-    (X1(:).^2).*(X2(:).^2) (X1(:).^3).*X2(:) X1(:).*(X2(:).^3) X1(:).^5 X2(:).^5 (X1(:).^4).*X2(:) X1(:).*(X2(:).^4) (X1(:).^3).*(X2(:).^2) (X1(:).^2).*(X2(:).^3)...
+    (X1(:).^3).*X2(:) X1(:).*(X2(:).^3) (X1(:).^2).*(X2(:).^2)  X1(:).^5 X2(:).^5 (X1(:).^4).*X2(:) X1(:).*(X2(:).^4) (X1(:).^3).*(X2(:).^2) (X1(:).^2).*(X2(:).^3)...
     X1(:).^6 X2(:).^6 (X1(:).^5).*X2(:) X1(:).*(X2(:).^5) (X1(:).^4).*(X2(:).^2) (X1(:).^2).*(X2(:).^4) (X1(:).^3).*(X2(:).^3)];
 
 [theta_E, std_theta_E, q_E, y_hat_E, epsilon_E, SSR_E, y_hat_E_ext, y_hat_E_ext_mat] = identificazioneModello(phi_E, phi_E_ext, X1, y_id_dt);
@@ -287,30 +287,30 @@ colorbar
 colormap('default')
 
 %Terzo grado
-phi_B_Val=[ones(n,1) x1_val x2_val x1_val.^2 x2_val.^2 x1_val.*x2_val x1_val.^3 x2_val.^3 (x1_val.^2).*x2_val];
-[yhat3Val, epsilon3Val, SSR3Val] = crossvalidazioneModello(phi_B_Val, theta_B, y_val_dt);
+phi_B_Val=[ones(n,1) x1_val x2_val x1_val.^2 x2_val.^2 x1_val.*x2_val x1_val.^3 x2_val.^3 (x1_val.^2).*x2_val x1_val.*(x2_val.^2)];
+[yhat3Val, epsilon3Val, SSR3Val] = crossvalidazioneModello(phi_B_Val, theta_B, y_val);
 
 %Quarto grado
-phi_C_Val=[ones(n,1) x1_val x2_val x1_val.^2 x2_val.^2 x1_val.*x2_val x1_val.^3 x2_val.^3 (x1_val.^2).*x2_val x1_val.*(x2_val.^2) x1_val.^4 x2_val.^4 (x1_val.^2).*(x2_val.^2) (x1_val.^3).*x2_val x1_val.*(x2_val.^3)];
-[yhat4Val, epsilon4Val, SSR4Val] = crossvalidazioneModello(phi_C_Val, theta_C, y_val_dt);
+phi_C_Val=[ones(n,1) x1_val x2_val x1_val.^2 x2_val.^2 x1_val.*x2_val x1_val.^3 x2_val.^3 (x1_val.^2).*x2_val x1_val.*(x2_val.^2) x1_val.^4 x2_val.^4 (x1_val.^3).*x2_val x1_val.*(x2_val.^3) (x1_val.^2).*(x2_val.^2)];
+[yhat4Val, epsilon4Val, SSR4Val] = crossvalidazioneModello(phi_C_Val, theta_C, y_val);
 
 %Quinto grado
-phi_D_Val=[ones(n,1) x1_val x2_val x1_val.^2 x2_val.^2 x1_val.*x2_val x1_val.^3 x2_val.^3 (x1_val.^2).*x2_val x1_val.*(x2_val.^2) x1_val.^4 x2_val.^4 (x1_val.^2).*(x2_val.^2) (x1_val.^3).*x2_val x1_id.*(x2_val.^3) ...
-     x2_val.^5 (x1_val.^4).*x2_val x1_val.*(x2_val.^4) (x1_val.^3).*(x2_val.^2) (x1_val.^2).*(x2_val.^3)];
-[yhat5Val, epsilon5Val, SSR5Val] = crossvalidazioneModello(phi_D_Val, theta_D, y_val_dt);
+phi_D_Val=[ones(n,1) x1_val x2_val x1_val.^2 x2_val.^2 x1_val.*x2_val x1_val.^3 x2_val.^3 (x1_val.^2).*x2_val x1_val.*(x2_val.^2) x1_val.^4 x2_val.^4 (x1_val.^3).*x2_val x1_id.*(x2_val.^3) ...
+     (x1_val.^2).*(x2_val.^2) x1_val.^5  x2_val.^5 (x1_val.^4).*x2_val x1_val.*(x2_val.^4) (x1_val.^3).*(x2_val.^2) (x1_val.^2).*(x2_val.^3)];
+[yhat5Val, epsilon5Val, SSR5Val] = crossvalidazioneModello(phi_D_Val, theta_D, y_val);
 
 %Sesto grado
-phi_E_Val=[ones(n,1) x1_val x2_val x1_val.^2 x2_val.^2 x1_val.*x2_val x1_val.^3 x1_val.^3 (x1_val.^2).*x2_val x1_val.*(x1_val.^2) x1_val.^4 x2_val.^4 (x1_val.^2).*(x1_val.^2) (x1_val.^3).*x2_val x1_val.*(x2_val.^3) ...
-     x1_val.^5 x2_val.^5 (x1_val.^4).*x2_val x1_val.*(x2_val.^4) (x1_val.^3).*(x2_val.^2) (x1_val.^2).*(x2_val.^3)...
+phi_E_Val=[ones(n,1) x1_val x2_val x1_val.^2 x2_val.^2 x1_val.*x2_val x1_val.^3 x1_val.^3 (x1_val.^2).*x2_val x1_val.*(x1_val.^2) x1_val.^4 x2_val.^4 (x1_val.^3).*x2_val x1_val.*(x2_val.^3) ...
+     (x1_val.^2).*(x1_val.^2) x1_val.^5 x2_val.^5 (x1_val.^4).*x2_val x1_val.*(x2_val.^4) (x1_val.^3).*(x2_val.^2) (x1_val.^2).*(x2_val.^3)...
      x1_val.^6 x2_val.^6 x1_val.^5.*x2_val x1_val.*(x2_val.^5) (x1_val.^4).*(x2_val.^2) (x1_val.^2).*(x2_val.^4) (x1_val.^3).*(x2_val.^3)];
-[yhat6Val, epsilon6Val, SSR6Val] = crossvalidazioneModello(phi_E_Val, theta_E, y_val_dt);
+[yhat6Val, epsilon6Val, SSR6Val] = crossvalidazioneModello(phi_E_Val, theta_E, y_val);
 
 %Settimo grado
 phi_F_Val=[ones(n,1) x1_val x2_val x1_val.^2 x2_val.^2 x1_val.*x2_val x1_val.^3 x1_val.^3 (x1_val.^2).*x2_val x1_val.*(x1_val.^2) x1_val.^4 x2_val.^4 (x1_val.^2).*(x1_val.^2) (x1_val.^3).*x2_val x1_val.*(x2_val.^3) ...
      x1_val.^5 x2_val.^5 (x1_val.^4).*x2_val x1_val.*(x2_val.^4) (x1_val.^3).*(x2_val.^2) (x1_val.^2).*(x2_val.^3)...
      x1_val.^6 x2_val.^6 x1_val.^5.*x2_val x1_val.*(x2_val.^5) (x1_val.^4).*(x2_val.^2) (x1_val.^2).*(x2_val.^4) (x1_val.^3).*(x2_val.^3) x1_val.^7 x2_val.^7 x1_val.^6.*x2_val x1_val.*(x2_val.^6) (x1_val.^5).*(x2_val.^2) ...
      (x1_val.^2).*(x2_val.^5) (x1_val.^4).*(x2_val.^3) (x1_val.^3).*(x2_val.^4)];
-[yhat7Val, epsilon7Val, SSR7Val] = crossvalidazioneModello(phi_F_Val, theta_F, y_val_dt);
+[yhat7Val, epsilon7Val, SSR7Val] = crossvalidazioneModello(phi_F_Val, theta_F, y_val);
 
 %Ottavo grado
 phi_G_Val=[ones(n,1) x1_val x2_val x1_val.^2 x2_val.^2 x1_val.*x2_val x1_val.^3 x1_val.^3 (x1_val.^2).*x2_val x1_val.*(x1_val.^2) x1_val.^4 x2_val.^4 (x1_val.^2).*(x1_val.^2) (x1_val.^3).*x2_val x1_val.*(x2_val.^3) ...
@@ -318,7 +318,7 @@ phi_G_Val=[ones(n,1) x1_val x2_val x1_val.^2 x2_val.^2 x1_val.*x2_val x1_val.^3 
      x1_val.^6 x2_val.^6 x1_val.^5.*x2_val x1_val.*(x2_val.^5) (x1_val.^4).*(x2_val.^2) (x1_val.^2).*(x2_val.^4) (x1_val.^3).*(x2_val.^3) x1_val.^7 x2_val.^7 x1_val.^6.*x2_val x1_val.*(x2_val.^6) (x1_val.^5).*(x2_val.^2) ...
      (x1_val.^2).*(x2_val.^5) (x1_val.^4).*(x2_val.^3) (x1_val.^3).*(x2_val.^4) x1_val.^8 x2_val.^8 x1_val.^7.*x2_val x1_val.*(x2_val.^7) (x1_val.^6).*(x2_val.^2) ...
      (x1_val.^2).*(x2_val.^6) (x1_val.^5).*(x2_val.^3) (x1_val.^3).*(x2_val.^5) (x1_val.^4).*(x2_val.^4)];
-[yhat8Val, epsilon8Val, SSR8Val] = crossvalidazioneModello(phi_G_Val, theta_G, y_val_dt);
+[yhat8Val, epsilon8Val, SSR8Val] = crossvalidazioneModello(phi_G_Val, theta_G, y_val);
 
 %Nono grado
 phi_H_Val=[ones(n,1) x1_val x2_val x1_val.^2 x2_val.^2 x1_val.*x2_val x1_val.^3 x1_val.^3 (x1_val.^2).*x2_val x1_val.*(x1_val.^2) x1_val.^4 x2_val.^4 (x1_val.^2).*(x1_val.^2) (x1_val.^3).*x2_val x1_val.*(x2_val.^3) ...
@@ -327,7 +327,7 @@ phi_H_Val=[ones(n,1) x1_val x2_val x1_val.^2 x2_val.^2 x1_val.*x2_val x1_val.^3 
      (x1_val.^2).*(x2_val.^5) (x1_val.^4).*(x2_val.^3) (x1_val.^3).*(x2_val.^4) x1_val.^8 x2_val.^8 x1_val.^7.*x2_val x1_val.*(x2_val.^7) (x1_val.^6).*(x2_val.^2) ...
      (x1_val.^2).*(x2_val.^6) (x1_val.^5).*(x2_val.^3) (x1_val.^3).*(x2_val.^5) (x1_val.^4).*(x2_val.^4) x1_val.^9 x2_val.^9 x1_val.^8.*x2_val x1_val.*(x2_val.^8) (x1_val.^7).*(x2_val.^2) ...
      (x1_val.^2).*(x2_val.^7) (x1_val.^6).*(x2_val.^3) (x1_val.^3).*(x2_val.^6) (x1_val.^5).*(x2_val.^4) (x1_val.^4).*(x2_val.^5)];
-[yhat9Val, epsilon9Val, SSR9Val] = crossvalidazioneModello(phi_H_Val, theta_H, y_val_dt);
+[yhat9Val, epsilon9Val, SSR9Val] = crossvalidazioneModello(phi_H_Val, theta_H, y_val);
 
 %Decimo grado
 phi_I_Val=[ones(n,1) x1_val x2_val x1_val.^2 x2_val.^2 x1_val.*x2_val x1_val.^3 x1_val.^3 (x1_val.^2).*x2_val x1_val.*(x1_val.^2) x1_val.^4 x2_val.^4 (x1_val.^2).*(x1_val.^2) (x1_val.^3).*x2_val x1_val.*(x2_val.^3) ...
@@ -337,7 +337,7 @@ phi_I_Val=[ones(n,1) x1_val x2_val x1_val.^2 x2_val.^2 x1_val.*x2_val x1_val.^3 
      (x1_val.^2).*(x2_val.^6) (x1_val.^5).*(x2_val.^3) (x1_val.^3).*(x2_val.^5) (x1_val.^4).*(x2_val.^4) x1_val.^9 x2_val.^9 x1_val.^8.*x2_val x1_val.*(x2_val.^8) (x1_val.^7).*(x2_val.^2) ...
      (x1_val.^2).*(x2_val.^7) (x1_val.^6).*(x2_val.^3) (x1_val.^3).*(x2_val.^6) (x1_val.^5).*(x2_val.^4) (x1_val.^4).*(x2_val.^5) x1_val.^10 x2_val.^10 x1_val.^9.*x2_val x1_val.*(x2_val.^9) (x1_val.^8).*(x2_val.^2) ...
      (x1_val.^2).*(x2_val.^8) (x1_val.^7).*(x2_val.^3) (x1_val.^3).*(x2_val.^7) (x1_val.^6).*(x2_val.^4) (x1_val.^4).*(x2_val.^6) (x1_val.^5).*(x2_val.^5)];
-[yhat10Val, epsilon10Val, SSR10Val] = crossvalidazioneModello(phi_I_Val, theta_I, y_val_dt);
+[yhat10Val, epsilon10Val, SSR10Val] = crossvalidazioneModello(phi_I_Val, theta_I, y_val);
 
 %Undicesimo grado
 phi_L_Val=[ones(n,1) x1_val x2_val x1_val.^2 x2_val.^2 x1_val.*x2_val x1_val.^3 x1_val.^3 (x1_val.^2).*x2_val x1_val.*(x1_val.^2) x1_val.^4 x2_val.^4 (x1_val.^2).*(x1_val.^2) (x1_val.^3).*x2_val x1_val.*(x2_val.^3) ...
@@ -348,7 +348,7 @@ phi_L_Val=[ones(n,1) x1_val x2_val x1_val.^2 x2_val.^2 x1_val.*x2_val x1_val.^3 
      (x1_val.^2).*(x2_val.^7) (x1_val.^6).*(x2_val.^3) (x1_val.^3).*(x2_val.^6) (x1_val.^5).*(x2_val.^4) (x1_val.^4).*(x2_val.^5) x1_val.^10 x2_val.^10 x1_val.^9.*x2_val x1_val.*(x2_val.^9) (x1_val.^8).*(x2_val.^2) ...
      (x1_val.^2).*(x2_val.^8) (x1_val.^7).*(x2_val.^3) (x1_val.^3).*(x2_val.^7) (x1_val.^6).*(x2_val.^4) (x1_val.^4).*(x2_val.^6) (x1_val.^5).*(x2_val.^5) x1_val.^11 x2_val.^11 x1_val.^10.*x2_val x1_val.*(x2_val.^10) (x1_val.^9).*(x2_val.^2) ...
      (x1_val.^2).*(x2_val.^9) (x1_val.^8).*(x2_val.^3) (x1_val.^3).*(x2_val.^8) (x1_val.^7).*(x2_val.^4) (x1_val.^4).*(x2_val.^7) (x1_val.^6).*(x2_val.^5) (x1_val.^6).*(x2_val.^5)];
-[yhat11Val, epsilon11Val, SSR11Val] = crossvalidazioneModello(phi_L_Val, theta_L, y_val_dt);
+[yhat11Val, epsilon11Val, SSR11Val] = crossvalidazioneModello(phi_L_Val, theta_L, y_val);
 
 %Dodicesimo grado
 phi_M_Val=[ones(n,1) x1_val x2_val x1_val.^2 x2_val.^2 x1_val.*x2_val x1_val.^3 x1_val.^3 (x1_val.^2).*x2_val x1_val.*(x1_val.^2) x1_val.^4 x2_val.^4 (x1_val.^2).*(x1_val.^2) (x1_val.^3).*x2_val x1_val.*(x2_val.^3) ...
@@ -360,7 +360,7 @@ phi_M_Val=[ones(n,1) x1_val x2_val x1_val.^2 x2_val.^2 x1_val.*x2_val x1_val.^3 
      (x1_val.^2).*(x2_val.^8) (x1_val.^7).*(x2_val.^3) (x1_val.^3).*(x2_val.^7) (x1_val.^6).*(x2_val.^4) (x1_val.^4).*(x2_val.^6) (x1_val.^5).*(x2_val.^5) x1_val.^11 x2_val.^11 x1_val.^10.*x2_val x1_val.*(x2_val.^10) (x1_val.^9).*(x2_val.^2) ...
      (x1_val.^2).*(x2_val.^9) (x1_val.^8).*(x2_val.^3) (x1_val.^3).*(x2_val.^8) (x1_val.^7).*(x2_val.^4) (x1_val.^4).*(x2_val.^7) (x1_val.^6).*(x2_val.^5) (x1_val.^6).*(x2_val.^5) x1_val.^12 x2_val.^12 x1_val.^11.*x2_val x1_val.*(x2_val.^11) (x1_val.^10).*(x2_val.^2) ...
      (x1_val.^2).*(x2_val.^10) (x1_val.^9).*(x2_val.^3) (x1_val.^3).*(x2_val.^9) (x1_val.^8).*(x2_val.^4) (x1_val.^4).*(x2_val.^8) (x1_val.^7).*(x2_val.^5) (x1_val.^5).*(x2_val.^7) (x1_val.^6).*(x2_val.^6)];
-[yhat12Val, epsilon12Val, SSR12Val] = crossvalidazioneModello(phi_M_Val, theta_M, y_val_dt);
+[yhat12Val, epsilon12Val, SSR12Val] = crossvalidazioneModello(phi_M_Val, theta_M, y_val);
 
 %Tredicesimo grado
 phi_N_Val=[ones(n,1) x1_val x2_val x1_val.^2 x2_val.^2 x1_val.*x2_val x1_val.^3 x1_val.^3 (x1_val.^2).*x2_val x1_val.*(x1_val.^2) x1_val.^4 x2_val.^4 (x1_val.^2).*(x1_val.^2) (x1_val.^3).*x2_val x1_val.*(x2_val.^3) ...
@@ -373,4 +373,4 @@ phi_N_Val=[ones(n,1) x1_val x2_val x1_val.^2 x2_val.^2 x1_val.*x2_val x1_val.^3 
      (x1_val.^2).*(x2_val.^9) (x1_val.^8).*(x2_val.^3) (x1_val.^3).*(x2_val.^8) (x1_val.^7).*(x2_val.^4) (x1_val.^4).*(x2_val.^7) (x1_val.^6).*(x2_val.^5) (x1_val.^6).*(x2_val.^5) x1_val.^12 x2_val.^12 x1_val.^11.*x2_val x1_val.*(x2_val.^11) (x1_val.^10).*(x2_val.^2) ...
      (x1_val.^2).*(x2_val.^10) (x1_val.^9).*(x2_val.^3) (x1_val.^3).*(x2_val.^9) (x1_val.^8).*(x2_val.^4) (x1_val.^4).*(x2_val.^8) (x1_val.^7).*(x2_val.^5) (x1_val.^5).*(x2_val.^7) (x1_val.^6).*(x2_val.^6) x1_val.^13 x2_val.^13 x1_val.^12.*x2_val x1_val.*(x2_val.^12) (x1_val.^11).*(x2_val.^2) ...
      (x1_val.^2).*(x2_val.^11) (x1_val.^10).*(x2_val.^3) (x1_val.^3).*(x2_val.^10) (x1_val.^9).*(x2_val.^4) (x1_val.^4).*(x2_val.^9) (x1_val.^8).*(x2_val.^5) (x1_val.^5).*(x2_val.^8) (x1_val.^7).*(x2_val.^6) (x1_val.^6).*(x2_val.^7)];
-[yhat13Val, epsilon13Val, SSR13Val] = crossvalidazioneModello(phi_N_Val, theta_N, y_val_dt);
+[yhat13Val, epsilon13Val, SSR13Val] = crossvalidazioneModello(phi_N_Val, theta_N, y_val);
